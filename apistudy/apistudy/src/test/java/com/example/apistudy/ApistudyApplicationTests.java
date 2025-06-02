2b1f201c509d475aa1c0613e1df11a44219d6cc0
kjsHome <was1239@gmail.com>
2025-06-02 15:25:51 +0900

    Spring boot 프로젝트 파일

---
+++ apistudy/apistudy/src/test/java/com/example/apistudy/ApistudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApistudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
