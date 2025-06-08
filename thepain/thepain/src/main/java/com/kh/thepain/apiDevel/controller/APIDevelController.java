b03231f9cb0cbe986c92ad68f7332147010ea435
kjsHome <was1239@gmail.com>
2025-06-08 19:21:19 +0900

    채용공고 전체 ì¡내용 반환 메소드 구현

--- thepain/thepain/src/main/java/com/kh/thepain/apiDevel/controller/APIDevelController.java
+++ thepain/thepain/src/main/java/com/kh/thepain/apiDevel/controller/APIDevelController.java
@@ -1,14 +1,27 @@
 package com.kh.thepain.apiDevel.controller;
 
+import com.kh.thepain.postList.model.service.PostListServiceImpl;
+import com.kh.thepain.postList.model.vo.PostList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @ResponseBody
 public class APIDevelController {
 
+    @Autowired
+    private PostListServiceImpl pService;
+
+    /**
+     * 채용공고 정보 조회
+     * @return
+     */
     @RequestMapping(value="/jobs", method = RequestMethod.GET)
     @ResponseBody
-    public String testAPI(){
-        return "API 개발전 테스트";
+    public ArrayList<PostList> testAPI(){
+        ArrayList<PostList> jobPostListAPI = pService.selectJobPostList();
+        return jobPostListAPI;
     }
 }
