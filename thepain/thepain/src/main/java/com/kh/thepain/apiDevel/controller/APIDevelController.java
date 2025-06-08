f7e48f8fe82a034b5fb7496a4490503193c24f07
kjsHome <was1239@gmail.com>
2025-06-09 03:02:42 +0900

    특정 회사의 채용공고 조회 매ì소드 작성 중

--- thepain/thepain/src/main/java/com/kh/thepain/apiDevel/controller/APIDevelController.java
+++ thepain/thepain/src/main/java/com/kh/thepain/apiDevel/controller/APIDevelController.java
@@ -17,13 +17,14 @@ public class APIDevelController {
      * 채용공고 전체 정보 조회
      * @return
      */
-    @RequestMapping(value="/jobs", method = RequestMethod.GET)
+    @RequestMapping(value="/allJobList", method = RequestMethod.GET)
     //@ResponseBody -> @RestController가 있으면 셍략가능
     public ArrayList<PostList> selectAllJobPostList(){
         return pService.selectJobPostList();
     }
 
-    public void selectJobPost(){
-
+    @GetMapping(value="/jobList/{companyName}", method = RequestMethod.GET)
+    public void selectJobPost(@PathVariable String companyName){
+        //회사이름으로 회사번호 조회 후 회사번호로 채용담당자 마이페이지에서 채용공고 조회하는 메소드 찾아서 가쟈옴
     }
 }
