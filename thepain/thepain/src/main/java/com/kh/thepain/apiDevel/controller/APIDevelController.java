0689e0ef48f94679020ab8d4aa14e0ff1e23ad4b
kjsHome <was1239@gmail.com>
2025-06-09 01:35:58 +0900

    원하í하는 채용공고 조회 메소드 구ì성중

--- thepain/thepain/src/main/java/com/kh/thepain/apiDevel/controller/APIDevelController.java
+++ thepain/thepain/src/main/java/com/kh/thepain/apiDevel/controller/APIDevelController.java
@@ -8,20 +8,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@ResponseBody
 public class APIDevelController {
 
     @Autowired
     private PostListServiceImpl pService;
 
     /**
-     * 채용공고 정보 조회
+     * 채용공고 전체 정보 조회
      * @return
      */
     @RequestMapping(value="/jobs", method = RequestMethod.GET)
-    @ResponseBody
-    public ArrayList<PostList> testAPI(){
-        ArrayList<PostList> jobPostListAPI = pService.selectJobPostList();
-        return jobPostListAPI;
+    //@ResponseBody -> @RestController가 있으면 셍략가능
+    public ArrayList<PostList> selectAllJobPostList(){
+        return pService.selectJobPostList();
+    }
+
+    public void selectJobPost(){
+
     }
 }
