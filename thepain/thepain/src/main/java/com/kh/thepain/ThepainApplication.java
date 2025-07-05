8d2b987c11629ab06e3a99ccd6fd05b5ed14621c
kjsHome <was1239@gmail.com>
2025-07-05 13:11:27 +0900

    ãjsp 사ì용을 위해 war + 외부톰갯캣 사용을 유위한 설정 ë변경

--- thepain/thepain/src/main/java/com/kh/thepain/ThepainApplication.java
+++ thepain/thepain/src/main/java/com/kh/thepain/ThepainApplication.java
@@ -3,12 +3,22 @@ package com.kh.thepain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class ThepainApplication {
+public class ThepainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThepainApplication.class, args);
+	}
+
+	/*
+		jsp 사용을 위해 war + 외부 톰캣 사용을 위한 메소드
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThepainApplication.class);
 	}
 
 }
